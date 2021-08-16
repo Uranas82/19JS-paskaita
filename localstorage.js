00b192e556c102118a13c1249ddfb4bd88341b32
Uranas82 <uranas82@yahoo.co.uk>
2021-08-16 20:55:14 +0300

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -29,7 +29,7 @@ duomenys.forEach(item => {
         var card = `<div class="border border-1 shadow-sm p-3 mb-3  rounded todo-item">
         <h4 class="mb-3 input-name">${item.todo}</h4>
         <button type="button" class="btn btn-danger delete">Delete</button>
-        <button type="button" class="btn btn-success move-todo">Move to Done</button>
+        <button type="button" class="btn btn-success move-todo">Move Back</button>
         <button type="button" class="btn btn-warning edit" data-bs-toggle="modal" data-bs-target="#edit-modal">Edit</button>
     </div>`;
     doneList.innerHTML += card;
@@ -46,8 +46,7 @@ duomenys.forEach(item => {
 // Add todo
 todoForm.addEventListener('submit', e => {
     e.preventDefault();
-    var duomenys = localStorage.getItem('Sarasas');
-    duomenys = JSON.parse(duomenys);
+    // 
     if (todoInput.value === '') {
         todoInput.classList.add('is-invalid');
     } else {
@@ -59,8 +58,10 @@ todoForm.addEventListener('submit', e => {
         <button type="button" class="btn btn-warning edit" data-bs-toggle="modal" data-bs-target="#edit-modal">Edit</button>
     </div>`;
         todoList.innerHTML += newTodo;
-        duomenys.push({todo: todoInput.value, done: false});
-        localStorage.setItem('Sarasas', JSON.stringify(duomenys));
+        var localItems = JSON.parse(localStorage.getItem('Sarasas'));
+        var newTask = {todo: todoInput.value, done: false};
+        localItems.push(newTask);
+        localStorage.setItem('Sarasas', JSON.stringify(localItems));
         todoForm.reset();
     }
 });
@@ -69,44 +70,55 @@ todoForm.addEventListener('submit', e => {
 document.querySelector('body').addEventListener('click', e => {
     // Delete todo
     if (e.target.classList.contains('delete')) {
-        var duomenys = localStorage.getItem('Sarasas');
-        duomenys = JSON.parse(duomenys);
-        var text = e.target.closest('.todo-item').querySelector('h4').innerText;
-       
-        const nauji = duomenys.filter(todo => {
-            return todo != text;
-            });
-        duomenys = nauji;
         e.target.closest('.todo-item').remove();
-        localStorage.setItem('Sarasas', JSON.stringify(duomenys));
-        console.log(duomenys);
+        var localItems = JSON.parse(localStorage.getItem('Sarasas'));
+        var text = e.target.closest('.todo-item').querySelector('h4').innerText;
+        const nauji = localItems.filter(todo => {
+            return todo.todo != text;
+            });
+        // duomenys = nauji;
+        
+        localStorage.setItem('Sarasas', JSON.stringify(nauji));
         // move todo to done
     } else if (e.target.classList.contains('move-todo')) {
-        var duomenys = localStorage.getItem('Sarasas');
-        duomenys = JSON.parse(duomenys);
         doneList.appendChild(e.target.closest('.todo-item'));
         e.target.innerText = 'Move Back';
         e.target.classList.remove('move-todo');
         e.target.classList.add('move-done');
-        // move todo to list
+        var localItems = JSON.parse(localStorage.getItem('Sarasas'));
+        var text = e.target.closest('.todo-item').querySelector('h4').innerText;
+        const nauji = localItems.map(item => {
+            return item.todo == text ? {todo: item.todo, done: !item.done} : item;
+        });
+        localStorage.setItem ('Sarasas', JSON.stringify(nauji));
+ // move todo to list
     } else if (e.target.classList.contains('move-done')) {
-        var duomenys = localStorage.getItem('Sarasas');
-        duomenys = JSON.parse(duomenys);
+        // var duomenys = localStorage.getItem('Sarasas');
         todoList.appendChild(e.target.closest('.todo-item'));
         e.target.innerText = 'Move to Done';
         e.target.classList.add('move-todo');
         e.target.classList.remove('move-done');
+        var text = e.target.closest('.todo-item').querySelector('h4').innerText;
+        var localItems = JSON.parse(localStorage.getItem('Sarasas'));
+        const nauji = localItems.map(item => item.todo == text ? {...item, done: !item.done} : item);
+        localStorage.setItem('Sarasas', JSON.stringify(nauji));
         // Paspaudziam edit mygtuka
     } else if (e.target.classList.contains('edit')) {
         h4 = e.target.closest('.todo-item').querySelector('h4');
         var h4text = h4.innerText;
         editInput.value = h4text;
+    } else if (e.target.classList.contains('edit-submit')) {
+        var oldText = h4.innerText;
+        h4.innerText = editInput.value;
+        var localItems = JSON.parse(localStorage.getItem('Sarasas'));
+        const nauji = localItems.map(item => item.todo == oldText ? {...item, todo: editInput.value } : item);
+        localStorage.setItem('Sarasas', JSON.stringify(nauji));
     }
 });
 
 // confirm edit todo
-document.querySelector('.edit-submit').addEventListener('click', () => {
-    var duomenys = localStorage.getItem('Sarasas');
-    duomenys = JSON.parse(duomenys);
-    h4.innerText = editInput.value;
-});
+// document.querySelector('.edit-submit').addEventListener('click', () => {
+//     var duomenys = localStorage.getItem('Sarasas');
+//     duomenys = JSON.parse(duomenys);
+    
+// });
